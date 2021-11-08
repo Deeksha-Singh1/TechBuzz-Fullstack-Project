85b6af5371dcee37ee0fe1da93627902743f16dd
KhushiAgrawal <khushi.agarwal1_cs19@gla.ac.in>
2021-11-09 00:22:14 +0530

    update changes

---
+++ Quizzes/Question5.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "B is the only child of A. M and N are the children of B. D is the grandmother of N. How is A related to D?",
+    answer: "Husband",
+    options: [
+      "Grandmother",
+      "Husband",
+      "Mother",
+      "Son"
+    ]
+  },
+  {
+  numb: 2,
+    question: ". A man goes to the park and sees a girl who he recognized to be his relative. The girl was the daughter of his sister's husband-wife. How is the girl related to the man",
+    answer: "niece",
+    options: [
+      "niece",
+      "sister-in-law",
+      "sister",
+      "Aunt"
+    ]
+  },
+  {
+    numb: 3,
+      question: "There is a family of six-person P, Q, R, S, T, and U. They are Lawyers, Doctor, Teacher, Salesman, Engineer, and Accountant. There are two married couples in the family. S, the salesman is married to the Lady Teacher. The Doctor is married to the Lawyer U, The Accountant is the son of Q and brother of T. R, the Lawyer is the daughter-in-law of P. T is the unmarried Engineer. P is the Grandmother of U. Which is the profession of P?",
+      answer: "Teacher",
+      options: [
+        "Lawyer",
+        "Teacher",
+        "Doctor",
+        "Accountant"
+      ]
+    },
+    {
+     numb: 4,
+        question: "A lady runs 18km towards East and then24km towards North how far is she from her starting point and in which direction?",
+        answer: "30km north-east",
+        options: [
+        "42km North-east",
+        "42km east",
+        "6km east",
+        "30km north-east"
+      ]
+    },
+    {
+        numb: 5,
+           question: " All red is black.All black is pink. Conclusions: 1. Some black are red 2. Some pink are red  3. Some red are not pink 4. All pink are black",
+           answer: "Only 1 and 2",
+           options: [
+           "Only 1 and 2",
+           "Only 1 and 3",
+           "Only 1 and 4",
+           "Only 2 and 4"
+         ]
+       },
+       {
+        numb: 6,
+           question: "O,T,T,F,F,S,S_(what is the next)",
+           answer: "E",
+           options: [
+           "F",
+           "E",
+           "G",
+           "H"
+         ]
+       },
+       {
+        numb: 7,
+           question: "The oar is to rowboat as the foot is to",
+           answer: "skateboard",
+           options: [
+           "running",
+           "sneaker",
+           "skateboard",
+           "jumping"
+         ]
+       },
+       {
+        numb: 8,
+           question: "Statements: All phones are computers. All computers are calculators. Conclusions: 1. All calculators are computers 2. All phones are calculators 3. All computers are phones 4. Some calculators are phones",
+           answer: "Only 2 and 4",
+           options: [
+           "Only 1 and 4",
+           "Only 3 and 4",
+           "Only 2 and 4",
+           "Only 1 and 2"
+         ]
+       },
+       {
+        numb: 9,
+           question: "Pointing to a photograph of a boy Suresh said, He is the son of the only son of my mother. How is Suresh related to that boy?",
+           answer: "Father",
+           options: [
+           "Brother",
+           "Uncle",
+           "Cousin",
+           "Father"
+         ]
+       },
+       {
+        numb: 10,
+           question: "Reds are blues, blues are whites, whites are yellows, yellows are oranges, oranges are pinks, then what is the color of the sky?",
+           answer: "white",
+           options: [
+           "pinks",
+           "blue",
+           "white",
+           "yellow"
+         ]
+       },
+];
