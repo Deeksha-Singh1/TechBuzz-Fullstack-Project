e4746711a6a4794070afe092fde3e3781ec1a77f
Avanee Dheer <avanee.dheer_cs19@gla.ac.in>
2021-11-08 22:26:04 +0530

    update

---
+++ Quizzes/AptitudeTest1.js
@@ -0,0 +1,142 @@
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const option_list = document.querySelector(".option_list");
+const timeCount = quiz_box.querySelector(".timer .timer_sec");
+const timeLine = quiz_box.querySelector("header .time_line");
+
+//when start button is clicke d
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo"); //show info box
+}
+//when exit button is clicked
+exit_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide info box
+}
+
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.add("activeQuiz"); //show quiz box
+    showQuetions(0); //calling showQestions function
+    queCounter(1); //passing 1 parameter to queCounter
+    startTimer(15); //calling startTimer function
+    startTimerLine(0); //calling startTimerLine function
+}
+
+let que_count = 0;
+let que_numb = 1;
+let counter;
+let timeValue = 15;
+let widthValue=0;
+
+const next_btn = quiz_box.querySelector(".next_btn");
+const result_box = document.querySelector(".result_box");
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+// if Next Que button clicked
+next_btn.onclick = ()=>{
+    if(que_count < questions.length - 1){ //if question count is less than total question length
+        que_count++; //increment the que_count value
+        que_numb++; //increment the que_numb value
+        showQuetions(que_count); //calling showQestions function
+        queCounter(que_numb); //passing que_numb value to queCounter
+        clearInterval(counter);
+        startTimer(timeValue);
+        clearInterval(counterLine);
+        startTimerLine(widthValue);
+        next_btn.style.display = "none";
+    }
+    else{
+        console.log("Questions Completed");
+    }
+}
+
+
+function showQuetions(index){
+    const que_text = document.querySelector(".que_text");
+    //creating a new span and div tag for question and option and passing the value using array index
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    que_text.innerHTML = que_tag; //adding new span tag inside que_tag
+    option_list.innerHTML = option_tag; //adding new div tag inside option_tag
+    const option = option_list.querySelectorAll(".option");
+    // set onclick attribute to all available options
+    for(i=0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+let tickIcon = '<div class="icon tick"><i class="fa fa-check"></i></div>';
+let crossIcon = '<div class="icon cross"><i class="fa fa-times"></i></div>';
+
+function optionSelected(answer){
+    clearInterval(counter);
+    clearInterval(counterLine);
+    let userAns = answer.textContent;
+    let correctAns = questions[que_count].answer;
+    let alloptions= option_list.children.length;
+    if(userAns == correctAns){
+        answer.classList.add("correct");
+        console.log("Answer is correct");
+        answer.insertAdjacentHTML("beforeend", tickIcon);
+    }
+    else{
+        answer.classList.add("incorrect");
+        console.log("Answer is wrong");
+        answer.insertAdjacentHTML("beforeend", crossIcon);
+        //if answer is incorrect then automatically selected tthe correct answer
+        for(let i=0; i< alloptions; i++){
+            if(option_list.children[i].textContent == correctAns){
+                option_list.children[i].setAttribute("class", "option correct");
+                option_list.children[i].insertAdjacentHTML("beforeend", tickIcon);
+            }
+        }
+    }
+
+    //once user selected disabled all options
+    for(let i=0; i< alloptions; i++){
+        option_list.children[i].classList.add("disabled");
+    }
+    next_btn.style.display = "block";
+}
+
+function startTimer(time){
+    counter=setInterval(timer, 1000);
+    function timer(){
+        timeCount.textContent = time;
+        time--;
+        if(time < 9){
+            let addZero= timeCount.textContent;
+            timeCount.textContent = "0" + addZero;
+        }
+        if(time < 0){
+            clearInterval(counter);
+            timeCount.textContent = "00";
+        }
+    }
+}
+function startTimerLine(time){
+    counterLine=setInterval(timer, 29);
+    function timer(){
+        time+=1;
+        timeLine.style.width = time + "px";
+        if(time > 549){
+            clearInterval(counterLine);
+        }
+    }
+}
+
+
+const bottom_ques_counter = document.querySelector(".total_que");
+
+function queCounter(index){
+    //creating a new span tag and passing the question number and total question
+    
+    let totalQueCounTag = '<span><p>'+ index +'</p> of <p>'+ questions.length +'</p> Questions</span>';
+    bottom_ques_counter.innerHTML = totalQueCounTag;  //adding new span tag inside bottom_ques_counter
+}
---
+++ Quizzes/Questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "The average age of a man and his son is 28 years. The ratio of their ages is 3 :1 respectively. What is the man's age?",
+    answer: "42 years",
+    options: [
+      "30 years",
+      "38 years",
+      "44 years",
+      "42 years"
+    ]
+  },
+  {
+  numb: 2,
+    question: "A cyclic quadrilateral ABCD is such that AB = BC, AD = DC, AC is perpendicular to BD and ∠CAD = θ, then find the ∠ABC.",
+    answer: "2θ",
+    options: [
+      "θ",
+      "θ/2",
+      "2θ",
+      "3θ"
+    ]
+  },
+  {
+    numb: 3,
+      question: "If tan θ + cot θ = 2, then the value of tan2θ + cot2θ is",
+      answer: "2",
+      options: [
+        "2",
+        "1",
+        "√2",
+        "θ"
+      ]
+    },
+    {
+     numb: 4,
+        question: "How many integers are there between 300 and 600 that are divisible by 9?",
+        answer: "33",
+        options: [
+        "33",
+        "31",
+        "28",
+        "25"
+      ]
+    },
+    {
+        numb: 5,
+           question: "What will be the ratio of petrol and kerosene in the final solution formed by mixing petrol and kerosene that are present in three identical vessels in the ratio 4:1,5:2 and 6 :1 respectively?",
+           answer: "83 : 22",
+           options: [
+           "166 : 22",
+           "83 : 22",
+           "83 : 44",
+           "78 : 55"
+         ]
+       },
+       {
+        numb: 6,
+           question: "The respective ratio of radii of two right circular cylinders (A & B) is 2 : 5. The respective ratio of the heights of cylinders A and B is 3 : 1. What is the respective ratio of volumes of cylinders A and B?",
+           answer: "12 : 25",
+           options: [
+           "12 : 25",
+           "9 : 25",
+           "9 : 20",
+           "3 : 5"
+         ]
+       },
+       {
+        numb: 7,
+           question: "Raja gives 30% of his salary to his mother, 40% of the remaining salary he invests in an insurance scheme and PPF in the respective ratio of 4 : 3 and the remaining he keeps in his bank account. If the difference between the amount he gives to his mother and that he invests in insurance scheme is Rs. 8400, how much is Raja’s salary?",
+           answer: "Rs. 60,000",
+           options: [
+           "Rs. 60,000",
+           "Rs. 64,000",
+           " Rs. 65,000",
+           "Rs. 54,000"
+         ]
+       },
+       {
+        numb: 8,
+           question: "A, B, C and D are four consecutive odd numbers and their average is 42. What is the product of B and D?",
+           answer: "1845",
+           options: [
+           "1860",
+           "1890",
+           "1845",
+           "1677"
+         ]
+       },
+       {
+        numb: 9,
+           question: "A basket contains 3 blue, 5 black and 3 red balls. If 2 balls are drawn at random, what is the probability that one is black and one is red?",
+           answer: "3/11",
+           options: [
+           "2/11",
+           "8/11",
+           "9/11",
+           "3/11"
+         ]
+       },
+       {
+        numb: 10,
+           question: "A man buys a land and gives for it 20 times the annual rent Find the rate of interest he gets for his money.",
+           answer: "24%",
+           options: [
+           "10%",
+           "24%",
+           "45%",
+           "18%"
+         ]
+       },
+];
