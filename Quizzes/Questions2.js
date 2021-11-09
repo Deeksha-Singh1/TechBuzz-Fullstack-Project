818dfe7050f2ff24ec7a111114efddcf99fdcfda
Deeksha-Singh1 <deeksha.singh1_cs19@gla.ac.in>
2021-11-09 20:04:36 +0530

    ch

---
+++ Quizzes/Questions2.js
@@ -0,0 +1,81 @@
+let questions = [
+  {
+  numb: 1,
+  question: " If ‘P X Q’ means ‘P is the mother of Q’, ‘P + Q’ means ‘Q is the sister of P’ and ‘P - Q’ means ‘P is the daughter of Q’, then how will ‘A is the aunt of B’ be denoted? (Take A, B and C as three variables)",
+  answer: "B – C + A",
+  options: [
+    " B + A – C",
+    " A x C – B",
+    "C – B + A",
+    "B – C + A"
+    
+  ]
+},
+{
+numb: 2,
+  question: "Arun walks 25 metres towards north, then turns to his left and walks 10 metres and then turns to his right and walks 15 metres. Again turning to his right, he walks 14 metres. What will be the shortest distance between his starting point and ending point?",
+  answer: "40.12",
+  options: [
+    "25.32",
+    "40.12",
+    "42.38",
+    "41.48"
+  ]
+},
+{
+  numb: 3,
+    question: " In this question, a word has been given and is followed by four other options. One of them cannot be formed using the letters from the given word. Find that word.",
+    answer: " Inspire",
+    options: [
+      "Nations",
+      "Pianist",
+      " Inspire",
+      "Aspirin"
+    ]
+  },
+  {
+   numb: 4,
+      question: "Which one of the below-given options would be a meaning full order of the following words 1. Grasshopper 2. Eagle 3. Frog 4. Grass 5. Snake",
+      answer: " 4, 1, 3, 5, 2",
+      options: [
+      " 4, 1, 3, 5, 2",
+      "4, 1, 5, 3, 2",
+      " 1, 4, 2, 3, 5",
+      "2, 5, 3, 1, 4"
+    ]
+  },
+  {
+      numb: 5,
+         question: "4, 20, 120, 840, ?, 6720 . Identify the number on the place of ‘?’",
+         answer: "6720",
+         options: [
+         "5880",
+         "5040",
+         "6720",
+         "7560"
+       ]
+     },
+     {
+      numb: 6,
+         question: "How many daughters does A have? I. P’s father ‘A’ has four children II. P has three sisters",
+         answer: "if the data even in both the statements together are not sufficient to answer the question",
+         options: [
+         "if the data in statement I alone are sufficient to answer the question",
+         "if the data in statement II alone are sufficient to answer the question",
+         "if the data even in both the statements together are not sufficient to answer the question",
+         "if the data either in I or II alone are sufficient to answer the question"
+         
+       ]
+     },
+     {
+      numb: 7,
+         question: "ab_bca_ _dab_d_ Fill the blanks.",
+         answer: "abcce",
+         options: [
+         "aabce",
+         "abcce",
+         "  ccbce",
+         " abcde"
+       ]
+     }
+]
